25ed44dde609ce9ced400073f4fc1fc540217b3d
Fardowsamohamed <muniiramohamed096@gmail.com>
2023-06-15 18:11:05 +0300

    Added a print statement

---
+++ helworld.java
@@ -0,0 +1,10 @@
+package GitRevert;
+
+public class helworld {
+    public static void main(String[] args) {
+        int x =19;
+        int y =10;
+        System.out.println(x+y);
+        
+    }
+}
